532412b57a00a1fa06fe0e6b5e040601e8afb797
aleksey25 <aleksey25@csltd.com.ua>
2022-11-08 16:30:18 +0200

    Еще кусочек

---
+++ TestNGTests/src/test/java/com/testng/tetspackage4/TestsWithParams2.java
@@ -0,0 +1,17 @@
+package com.testng.tetspackage4;
+
+import demo.Tests;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class TestsWithParams2 {
+
+
+    //@Parameters ({"x", "a"})
+    @Parameters ({"x"})
+    @Test(testName = "Тест с параметром сьюта и без параметров теста")
+    public void TestWithSuiteParam(String x){
+    //public void TestWithSuiteParam(String x, String a){
+        Tests.testMethod("с параметром сьюта " + x);
+    }
+}
--- TestNGTests/src/test/java/demo/Tests.java
+++ TestNGTests/src/test/java/demo/Tests.java
@@ -16,5 +16,21 @@ public class Tests {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-    };
+    }
+
+
+    public static void failTestMethod(String testName) {
+        try {
+            System.out.println("Тест " + testName + " начат");
+            for (int i = 0; i < 5; i++) {
+                System.out.println(".");
+                Thread.sleep(200);
+            }
+            Assert.assertEquals(testName, "testName");
+            System.out.println("Тест " + testName + " закончен");
+            System.out.println("--------------------------------\n");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
