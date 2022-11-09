d8d4f6a53d58b0fd0b47a630720b1b2f1af69d8f
aleksey25 <aleksey25@csltd.com.ua>
2022-11-09 10:50:04 +0200

    Правка

--- TestNGTests/src/test/java/com/testng/testpackage1_justrun/ThirdClass.java
+++ TestNGTests/src/test/java/com/testng/testpackage1_justrun/ThirdClass.java
@@ -4,11 +4,15 @@ import org.testng.annotations.Test;
 
 import static demo.Tests.testMethod;
 
-
+@Test(testName = "Tests in separate class")
+// Все паблик методы по сути отмечены аннотацией тест
 public class ThirdClass {
 
-    @Test(testName = "Тест 1 в сьюте 2 (класс 3)")
     public void testInP2(){
-        testMethod("в пакете 2");
+        testMethod("1 в отдельном классе 2");
+    }
+
+    public void testInP2_2(){
+        testMethod("2 в отдельном классе 2");
     }
 }
